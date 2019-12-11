46809dd22ef5859c046442bb7d6f38e438452d8d
NLlovet <nllovet20199@columbushs.com>
2019-12-11 16:23:41 -0500

    iteration 1 done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,22 +1,37 @@
 class Chronometer {
   constructor() {
     this.currentTime = 0;
-    this.intervalId;
+    this.intervalId = 0;
   }
   
   startClick() {
-    this.intervalId = setInterval(addSecond, 1000);
-    
-    function addSecond() {
-      let i = 0;
-      i++;
-      this.currentTime = i;
+    this.intervalId = setInterval( () => {
+       this.currentTime += 1;
+    }, 1000);
+  }  
+  
+   getMinutes() {
+    if (this.currentTime > 59) {
+      return Math.floor(this.currentTime / 60);
+      }
+     return 0;
     }
+
+    getSeconds() {      
+      return this.currentTime % 60;
+    }
+
+  twoDigitsNumber(value) {
+    
+  return (value < 10) ? '0' + value.toString() : value.toString();
+
+    return (`${seconds.toString}`);
   }
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  stopClick() {
+    this.intervalId = clearInterval();
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
 }
